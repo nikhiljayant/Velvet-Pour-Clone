900528611bca1f166bc6f6b62f3f5b3668493f86
nikhiljayant <njayant86@gmail.com>
2025-07-27 00:26:06 +0530

    about section completed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,40 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { SplitText } from "gsap/all";
+
 const About = () => {
+  useGSAP(() => {
+    const titleSplit = SplitText.create("#about h2", {
+      type: "words",
+    });
+
+    const scrollTimeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#about",
+        start: "top center",
+      },
+    });
+
+    scrollTimeline
+      .from(titleSplit.words, {
+        opacity: 0,
+        duration: 1,
+        yPercent: 100,
+        ease: "expo.out",
+        stagger: 0.02,
+      })
+      .from(
+        ".top-grid div, .bottom-grid div",
+        {
+          opacity: 0,
+          duration: 1,
+          ease: "power1.inout",
+          stagger: 0.04,
+        },
+        "-=0.5" // It will start this animation 0.5 seconds before the previous one ends
+      );
+  });
+
   return (
     <div id="about">
       <div className="mb-16 md:px-0 px-5">
@@ -27,8 +63,34 @@ const About = () => {
             </div>
           </div>
         </div>
+      </div>
 
-        
+      <div className="top-grid">
+        <div className="md:col-span-3">
+          <div className="noisy" />
+          <img src="/images/abt1.png" alt="grid-img-1" />
+        </div>
+
+        <div className="md:col-span-6">
+          <div className="noisy" />
+          <img src="/images/abt2.png" alt="grid-img-2" />
+        </div>
+
+        <div className="md:col-span-3">
+          <div className="noisy" />
+          <img src="/images/abt5.png" alt="grid-img-5" />
+        </div>
+      </div>
+
+      <div className="bottom-grid">
+        <div className="md:col-span-8">
+          <div className="noisy" />
+          <img src="/images/abt3.png" alt="grid-img-3" />
+        </div>
+        <div className="md:col-span-4">
+          <div className="noisy" />
+          <img src="/images/abt4.png" alt="grid-img-4" />
+        </div>
       </div>
     </div>
   );
